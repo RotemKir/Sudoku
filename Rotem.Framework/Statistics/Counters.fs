1c3aa61d61105fc2866faff5103674f8297d1a6b
Rotem Kirshenbaum <rotemkir@gmail.com>
2017-11-26 21:14:33 +0200

    Initial counters export implementation

--- Rotem.Framework/Statistics/Counters.fs
+++ Rotem.Framework/Statistics/Counters.fs
@@ -14,12 +14,14 @@ module Counters =
 
     type Counters = Map<CounterName, CounterValue>
 
+    type TargetCounters = seq<CounterName * CounterValue>
+
     // Private functions
     
     let private setCounterValue counterName (counters : Counters) value =
         Map.add counterName value counters
 
-    let private setCounterStrategy initialValue getNextValue counterName counters : Counters =
+    let private incrementCounterStrategy initialValue getNextValue counterName counters : Counters =
        let setCounter = setCounterValue counterName counters
        match Map.containsKey counterName counters with
        | true -> 
@@ -51,12 +53,93 @@ module Counters =
 
     // Public functions
 
-    let incrementCounterByAmount amount = setCounterStrategy <| initialNumberCounterValue amount <| getNextNumberCounterValue amount
+    let incrementCounterByAmount amount = incrementCounterStrategy <| initialNumberCounterValue amount <| getNextNumberCounterValue amount
     
     let incrementCounter = incrementCounterByAmount 1
 
-    let incrementDiffCounter amount = setCounterStrategy <| initialDiffCounterValue amount <| getNextDiffCounterValue amount
+    let incrementDiffCounter amount = incrementCounterStrategy <| initialDiffCounterValue amount <| getNextDiffCounterValue amount
 
-    let incrementTimeCounter = setCounterStrategy <| initialTimeCounterValue <| getNextTimeCounterValue
+    let incrementTimeCounter = incrementCounterStrategy <| initialTimeCounterValue <| getNextTimeCounterValue
     
     let createStatistics : Counters = Map.empty<CounterName, CounterValue>
+
+    let getCounterValue counterValue =
+            match counterValue with
+            | NumberCounter value -> value
+            | DiffCounter (_, value) -> value
+            | TimeCounter (_, value) -> value.TotalMilliseconds |> int
+        
+    module Export =
+        open System.IO
+
+        // Types
+        
+        type StatCellValue = NumberCell of int | StringCell of string
+
+        type StatCell = 
+            {
+                CounterName : string
+                CellValue : StatCellValue
+            }
+
+        // Private functions 
+
+        let private writeToFile fileName contents = File.WriteAllText(fileName, contents)
+
+        let private getTargetCells counters =
+            Seq.map (fun getter -> getter counters)
+
+        let private getCountersByTarget (counters : Counters) =
+            counters
+            |> Map.toSeq
+            |> Seq.groupBy (fun (counterName, _) -> snd counterName)
+            
+        let private export exporter cellGetters counters : unit =
+            counters
+            |> getCountersByTarget 
+            |> Seq.map (fun (_, targetCounters) ->  getTargetCells targetCounters cellGetters)
+            |> exporter
+                   
+        let private getCsvRow getValues (cells : seq<StatCell>) = 
+            String.Join(",", Seq.map getValues cells)
+
+        let private getCsvHeader cells = 
+            cells
+            |> Seq.head
+            |> getCsvRow (fun cell -> cell.CounterName)
+
+        let private getCellDisplayValue cell =
+            match cell with
+            | {CounterName = _ ; CellValue = StringCell s} -> s
+            | {CounterName = _ ; CellValue = NumberCell n} -> string n
+
+        let private getCsvLine = getCsvRow getCellDisplayValue 
+
+        let private getCsvLines = Seq.map getCsvLine
+        
+        let private csvExporter fileName cells = 
+            Seq.fold (fun s line -> s + "\n" + line ) 
+            <| getCsvHeader cells
+            <| getCsvLines cells
+            |> writeToFile fileName
+
+        // Public functions 
+
+        let exportToCsv fileName = csvExporter fileName |> export 
+
+        let getTargetNameCell (counters : TargetCounters) =
+            let counterName = counters |> Seq.head |> fst
+            { 
+                CounterName = "Target"
+                CellValue = counterName |> snd |> StringCell
+            }
+
+        let getCounterValueCell counterName (counters: TargetCounters) =
+            let counterValue = 
+                counters
+                |> Seq.find (fun (name, _) -> fst name = counterName)
+                |> snd
+            { 
+                CounterName = counterName
+                CellValue = counterValue |> getCounterValue |> NumberCell 
+            }
--- Sudoku.ConsoleApplication/Program.fs
+++ Sudoku.ConsoleApplication/Program.fs
@@ -4,12 +4,13 @@
 open Sudoku.Library
 open Rotem.Framework.Logging
 open System
+open Rotem.Framework.Counters.Export
 
 let createLogger fileName = Logger.create [ConsoleLogger.Log ; FileLogger.Create <| fileName + ".log"]
 
 let loadBoard fileName = fileName |> BoardParser.createFromFile 
 
-let printSolveResult board statistics =
+let printSolveResult board =
     if Board.isSolved board
     then sprintf "\nSolved board:\n%s" <| Board.print board
     else sprintf "\nFailed to solve board:\n%s" <| Board.print board
@@ -22,15 +23,25 @@ let init =
     sprintf "Solving board:\n%s\n" <| Board.print board |> logger
     (board, logger)
 
+let exportValues = 
+    [
+        getTargetNameCell
+        getCounterValueCell "Runs"
+        getCounterValueCell "Time" 
+        getCounterValueCell "Cells Found"
+    ]
+
+let export = exportToCsv "Statistics.csv" exportValues
+
 [<EntryPoint>]
 let main _ = 
     let (board, logger) = init
 
-    board 
-    |> Solver.solve logger
-    ||> printSolveResult 
-    |> logger
-        
+    let (solvedBoard, statistics) = board |> Solver.solve logger
+   
+    solvedBoard |> printSolveResult |> logger
+    statistics |> export  
+    
     Console.ReadLine() |> ignore
 
     0 // return an integer exit code
